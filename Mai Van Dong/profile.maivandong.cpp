3858f5598d0b860ede658a3727e0407951d05841
Đông Mai Văn <125658747+maidong26032002@users.noreply.github.com>
2023-03-07 12:30:46 +0700

    Add files via upload

---
+++ Mai Van Dong/profile.maivandong.cpp
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <title>profile Mai Văn Đông</title>
+    
+  </head>
+  <body>
+    <img width="100" src="https://www.google.com.vn/url?sa=i&url=https%3A%2F%2Ftapchimeovat.com%2Fhinh-goku-songoku-dep-sieu-xayda-tu-cap-1-den-cap-20-cap-100-727&psig=AOvVaw3Jlm0_pz35BBH7Z3nUQk3s&ust=1678250171476000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCPii7pz_yP0CFQAAAAAdAAAAABAE"/>
+    <h2>
+     Mai Văn Đông
+    </h2>
+    <h3>
+      from : thanh hóa
+    </h3>
+    <div>
+      <p>
+        Sở thích:
+      </p>
+      <ul>
+        <li>Ăn</li>
+        <li>Ngủ</li>
+        <li>Nghỉ</li>
+      </ul>
+    </div>
+    <footer>
+      <h3>
+        Thông tin liên lạc
+      </h3>
+      <ul>
+        <li>email:maidong263@gmail.com</li>
+        <li>facebook: <a href="https://www.facebook.com/profile.php?id=100016049142880" class="facebook-btn">Mai Văn Đông</a></li>
+        <li>sđt:0383421307</li>
+      </ul>
+    </footer>
+  </body>
+      
+      
+  </html>
